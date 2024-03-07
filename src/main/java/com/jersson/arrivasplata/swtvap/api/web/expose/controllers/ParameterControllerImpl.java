0e3d2fdb0b6c36672e99398bdecc24bcf884665a
Jersson Giomar Arrivasplata Rojas <jgrojas@stefanini.com>
2024-03-07 09:04:34 -0500

    Add files

--- src/main/java/com/jersson/arrivasplata/swtvap/api/web/expose/controllers/ParameterControllerImpl.java
+++ src/main/java/com/jersson/arrivasplata/swtvap/api/web/expose/controllers/ParameterControllerImpl.java
@@ -1,7 +1,6 @@
 package com.jersson.arrivasplata.swtvap.api.web.expose.controllers;
 import java.util.ArrayList;
 
-import org.hibernate.mapping.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
